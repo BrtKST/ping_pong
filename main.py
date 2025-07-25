1e145cb247ad291e2f81883b1657090faa106f90
BrtKST <b.kostuj1011@gmail.com>
2025-07-25 21:10:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import pygame
+import random
+import settings as st
+
+pygame.init()
+
+#window settings
+WIDTH = st.WIDTH
+HEIGHT = st.HEIGHT
+font = pygame.font.SysFont('arial', 50)
+
+#import settings test
+print(WIDTH, HEIGHT)
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+
+#speed variables
+VEL = st.VEL
+BALL_VEL = st.BALL_VEL
+
+
+def draw(player_1,player_2,score_1,score_2,ball):
+    WIN.fill(st.GREEN)
+    pygame.draw.rect(WIN,st.BLUE,player_1)
+    pygame.draw.rect(WIN,st.RED,player_2)
+
+    pygame.display.update()
+
+
+
+def main():
+    clock = pygame.time.Clock()
+    run_game = True
+
+    player_1 = pygame.Rect(45, HEIGHT/2 - st.player_height/2, st.player_width, st.player_height)
+    player_2 = pygame.Rect(WIDTH-45, HEIGHT/2 - st.player_height/2, st.player_width, st.player_height)
+    ball = pygame.Rect(WIDTH//2, HEIGHT//2, st.BALL_RADIUS, st.BALL_RADIUS)
+    ball_dx, ball_dy = random.choice([-1, 1]) * BALL_VEL, random.choice([-1, 1]) * BALL_VEL
+
+
+    score_1 = 0
+    score_2 = 0
+
+    while run_game:
+        clock.tick(st.FPS)
+        keys = pygame.key.get_pressed()
+
+        draw(player_1, player_2, score_1, score_2, ball)
+        print('test')
+
+        if keys[pygame.K_ESCAPE]:
+            run_game = False
+    pygame.quit()
+            
+
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,16 @@
+WIDTH = 800
+HEIGHT = 600
+
+# Kolory
+WHITE = (255, 255, 255)
+GREEN = (0, 128, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+FPS = 60
+VEL = 5
+BALL_VEL = 5
+
+player_height = 60
+player_width = 20
+BALL_RADIUS = 10
