ca68bc955e55f128f7446108ceb2a70499e385c2
BrtKST <b.kostuj1011@gmail.com>
2025-07-25 21:41:19 +0200

    players movement

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ def main():
     clock = pygame.time.Clock()
     run_game = True
 
+    #creating, resp sprites
     player_1 = pygame.Rect(45, HEIGHT/2 - st.player_height/2, st.player_width, st.player_height)
     player_2 = pygame.Rect(WIDTH-45, HEIGHT/2 - st.player_height/2, st.player_width, st.player_height)
     ball = pygame.Rect(WIDTH//2, HEIGHT//2, st.BALL_RADIUS, st.BALL_RADIUS)
     ball_dx, ball_dy = random.choice([-1, 1]) * BALL_VEL, random.choice([-1, 1]) * BALL_VEL
-
 
     score_1 = 0
     score_2 = 0
@@ -45,13 +45,29 @@ def main():
         keys = pygame.key.get_pressed()
 
         draw(player_1, player_2, score_1, score_2, ball)
-        print('test')
+        #print('test')
         
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     run_game = False
-            
+            if event.type == pygame.QUIT:
+                run_game = False
+
+        ball.x += ball_dx
+        ball.y += ball_dy
+        
+        #Players movement
+        if keys[pygame.K_w] and player_1.top > 0:
+            player_1.y -= VEL
+        if keys[pygame.K_s] and player_1.bottom < HEIGHT:
+            player_1.y += VEL 
+
+        if keys[pygame.K_UP] and player_2.top > 0:
+            player_2.y -= VEL
+        if keys[pygame.K_DOWN] and player_2.bottom < HEIGHT:
+            player_2.y += VEL 
+
     pygame.quit()
             
 
